33c75492a5198629891c0bc8fd0ad2653d162515
plyboicartier <carter.glynn14@gmail.com>
2025-07-29 22:20:38 -0400

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import SearchFilters from './components/SearchFilters';
 import StockTable from './components/StockTable';
 import { Stock, ColumnConfig, Column } from './types';
 import { AuthProvider } from './contexts/AuthContext';
+import { ThemeProvider } from './components/theme-provider';
 import LoginForm from './components/Auth/LoginForm';
 import ProtectedRoute from './components/Auth/ProtectedRoute';
 import Layout from './components/Layout';
@@ -14,26 +15,28 @@ import Dashboard from './pages/Dashboard';
 function App() {
 
   return (
-    <AuthProvider>
-      <Router>
-        <div className='min-h-screen bg-gray-50'>
-          <Routes>
-            <Route path='/login' element={<LoginForm />} />
-            <Route
-              path='/*'
-              element={
-                <ProtectedRoute>
-                  <Layout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate to='/dashboard' replace />} />
-              <Route path ='dashboard' element={<Dashboard />} />
-            </Route>
-          </Routes>
-        </div>
-      </Router>
-    </AuthProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <AuthProvider>
+        <Router>
+          <div className='min-h-screen bg-gray-50'>
+            <Routes>
+              <Route path='/login' element={<LoginForm />} />
+              <Route
+                path='/*'
+                element={
+                  <ProtectedRoute>
+                    <Layout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate to='/dashboard' replace />} />
+                <Route path ='dashboard' element={<Dashboard />} />
+              </Route>
+            </Routes>
+          </div>
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,16 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     host: '0.0.0.0', // Required for Docker to expose the server
     port: 3000, // Set Vite to run on port 3000
